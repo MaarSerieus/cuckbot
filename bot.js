4a1c21d010ca8354584ddebfba757c7e0cacfdc3
MaarSerieus <31286780+MaarSerieus@users.noreply.github.com>
2017-12-21 11:42:42 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,30 @@
+var Discord = require("discord.js");
+var bot = new Discord.Client();
+
+bot.on("ready", () => {
+    console.log("Op");
+    setInterval(function(){
+        setTimeout(function(){
+        bot.user.setGame("with your hoe.");
+        }, 15 * 1000);
+        setTimeout(function(){
+        bot.user.setGame("with this shit, fuck off.");
+        }, 30 * 1000);
+        setTimeout(function(){
+        bot.user.setGame("with a hot chick.");
+        }, 45 * 1000);
+        setTimeout(function(){
+        bot.user.setGame("with your nan.");
+        }, 60 * 1000);
+    }, 75 * 1000);
+});
+
+bot.on("guildMemberAdd", (user) => {
+    try {
+    user.guild.channels.find("name", "general").send(`Fuck you ${user}`);
+    } catch(e) {
+        console.log(`${e}`);
+    }
+});
+
+bot.login(process.env.TOKEN);
